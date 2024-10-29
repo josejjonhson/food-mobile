0c3a2796bc5106a27eb88176e20d552d9227af17
José Jonhson <josejjonhson@gmail.com>
2024-10-28 21:43:01 -0300

    algumas alterações

--- src/screens/login/login.jsx
+++ src/screens/login/login.jsx
@@ -1,5 +1,5 @@
 import { Text, TouchableOpacity, View } from "react-native";
-import { styles } from "./login.style.js"
+import { styles } from "./login.style.js";
 import Header from "../../components/header/header.jsx";
 import TextBox from "../../components/textbox/textbox.jsx";
 import Button from "../../components/button/button.jsx";
