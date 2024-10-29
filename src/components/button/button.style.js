ef0dc47d111e77e988637623d7f2822951d8b7c5
José Jonhson <josejjonhson@gmail.com>
2024-10-28 21:48:33 -0300

    alterações

--- src/components/button/button.style.js
+++ src/components/button/button.style.js
@@ -1,15 +1,15 @@
 import { COLORS, FONT_SIZE } from "../../constants/theme.js";
 
 export const styles = {
-    texto: {
+    btn:{
+        width: "100%",
+        backgroundColor: COLORS.red,
+        borderRadius: 6
+    },
+    texto:{
         fontSize: FONT_SIZE.md, 
         color: "white",
         padding: 14,
         textAlign: "center"
-    },
-    btn: {
-        width: "100%",
-        backgroundColor: COLORS.red,
-        borderRadius: 6
     }
 }
